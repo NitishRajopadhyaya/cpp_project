23c71642ef896dcceb27b7fa19bc8caaa7925fd6
Nitish Rajopadhyaya <nitishrocker220@gmailcom>
2021-05-18 14:33:35 +0545

    lab1

---
+++ lab1_famili/area_triangle1.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+#include<math.h>
+using namespace std;
+int main()
+{
+    int side1,side2,side3, base,height;
+    float area,s;
+     char ch;
+    cout<<" For right angle triangle press 'Y' else press any key";
+    cin>>ch;
+    if( ch=='y'||ch=='Y')
+      {
+           cout<<"Enter base"<<endl;
+           cin>>base;
+           cout<<"enter height"<<endl;
+           cin>>height;
+
+              area = (base*height)/2; 
+      }
+
+      else
+          {
+              cout<<"Enter value of side a"<<endl;
+             cin>>side1;
+             cout<<"Enter valu of side b"<<endl;
+             cin>>side2;
+             cout<<"Enter vlaue of side c"<<endl;
+             cin>>side3;
+             s=(side1+side2+side3)/2;
+             area =sqrt(s*(s-side1)*(s-side2)*(s-side3));
+           
+          }
+
+          cout<<"Area of given triangle is \t"<<area;
+
+
+}
