cedce8157f5ef84e276870e533554d40ccadbdbb
Nitish Rajopadhyaya <nitishrocker220@gmailcom>
2021-05-26 13:20:07 +0545

    loop struct

--- lab2_structure/struct2.cpp
+++ lab2_structure/struct2.cpp
@@ -15,18 +15,23 @@ int main()
     int i;
     for(i=0;i<3;i++)
         {
-            cout<<"enter name of \t"<<i+1<<"\t student"<<endl;
+            cout<<"enter name , roll number  and address of  \t"<<i+1<<"\t student"<<endl;
             cin>>s[i].name;
-            cout<<"enter roll \t"<<i+1<<"\t student"<<endl;
+            //cout<<"enter roll \t"<<i+1<<"\t student"<<endl;
             cin>>s[i].roll;
-            cout<<"enter address \t"<<i+1<<" \t student"<<endl;
+           // cout<<"enter address \t"<<i+1<<" \t student"<<endl;
             cin>>s[i].address;
          }
             cout<<"entered information"<<endl;
+            cout<<endl;
+             cout<<"name"<<"\t"<<"roll "<<"\t"<<"Address"<<endl;
+             cout<<endl;
          for(int j=0;j<3;j++)
             {
                 
                 //puts(s.name);
+
+               
                 cout<<s[j].name<<"\t"<<s[j].roll<<"\t"<<s[j].address<<endl;
              }
 }
