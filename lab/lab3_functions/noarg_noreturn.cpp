c5e80ffb37e0c358336b2158b40bae3dc5d0d908
Nitish Rajopadhyaya <nitishrocker220@gmailcom>
2021-06-02 13:50:54 +0545

    no argumnet and no return type

--- lab/lab3_functions/noarg_noreturn.cpp
+++ lab/lab3_functions/noarg_noreturn.cpp
@@ -1,13 +1,20 @@
 #include<iostream>
+#include<math.h>
 using namespace std;
  void area()
  {
-      float base , height ;
-     cout<<"enter base"<<endl;
-     cin>>base;
-     cout<<"enter height"<<endl;
-      cin>>height;
-    cout<<"area is \t"<<(base * height)/2;
+   float side1,side2,side3,ar,s;
+     cout<<"enter side 1"<<endl;
+     cin>>side1;
+     cout<<"enter side 2"<<endl;
+     cin>>side2;
+     cout<<"enter side 3"<<endl;
+     cin>>side3;
+
+     s=(side1+side2+side3)/2;
+             ar =sqrt(s*(s-side1)*(s-side2)*(s-side3));
+             cout<<"Area is \t"<<ar;
+ 
  }
  int main()
  {
