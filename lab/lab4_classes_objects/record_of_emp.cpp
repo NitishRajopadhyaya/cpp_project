ff3afc9cb252498519a67e81e8a10d0be52faa13
Nitish Rajopadhyaya <nitishrocker220@gmailcom>
2021-06-15 13:58:20 +0545

    Employee records using class

---
+++ lab/lab4_classes_objects/record_of_emp.cpp
@@ -0,0 +1,51 @@
+#include<iostream>
+#include<stdio.h>
+using namespace std;
+ 
+ class Employee
+ {
+     private:
+       char name[20];
+       int id ;
+       float salary;
+
+     public:
+
+           void inputdata()
+            {
+                cout<<"Enter Your name "<<endl;
+                cin>>name;
+                cout<<"Enter Your id"<<endl;
+                cin>>id;
+                cout<<"Enter your salary"<<endl;
+                cin>>salary;
+            }  
+
+
+            void displaydata()
+            {
+                
+                
+                cout<<name<<"\t"<<id<<"\t"<<salary;
+                
+                
+                
+            }
+ };
+
+ int main()
+ {
+     Employee E[5];;
+     for(int i=0;i<5;i++)
+       {
+           E[i].inputdata();
+       }
+  cout<<"Name \t"<<" Id \t"<<"salary \t"<<endl;
+     for(int j=0;j<5;j++)
+     {
+         E[j].displaydata();
+         cout<<endl;
+     }
+
+     
+ }
