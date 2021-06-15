b987e97671ba523aca381489f5bbde21de27f8a3
Nitish Rajopadhyaya <nitishrocker220@gmailcom>
2021-06-15 14:00:12 +0545

    calulation of SI

---
+++ lab/lab4_classes_objects/simple_interest.cpp
@@ -0,0 +1,64 @@
+#include<iostream>
+using namespace std;
+class Interest
+{
+    private:
+      float rate,SI;
+
+    public:
+        Interest()
+         {
+             rate=15;
+         } 
+
+       void calulation(double per , double tim , double r)
+        {
+             
+            
+            SI=(per*tim*r)/100;
+        }     
+
+         void calulation(double per , double tim )
+        {
+             
+            
+            SI=(per*tim*rate)/100;
+        }   
+
+        void display()
+          {
+              cout<<"Simple interest is : \t"<<SI;
+          }                  
+      
+};
+
+int main()
+{
+    Interest I1;
+      float percentage,rate1,time;
+      char ch;
+        cout<<"Enter percentage";
+        cin>>percentage;
+        cout<<"Enter time";
+        cin>>time;
+        cout<<"Do you want to enter rate Y/N"<<endl;
+        cin>>ch;
+        
+                if(ch=='Y'|| ch=='y')
+                {
+                    cout<<"enter rate";
+                    cin>>Rate1;
+                    I1.calulation(percentage,time,rate1);
+                }
+
+
+                else
+                    I1.calulation(percentage,time);
+
+
+       I1.display();
+
+       /*I1.calulation(10.0,1,10.0);
+       I1.calulation(10.0,10.0);
+       I1.display();*/
+}
