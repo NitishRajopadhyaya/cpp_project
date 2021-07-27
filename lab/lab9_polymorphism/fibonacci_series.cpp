2c62325e61ab69b32e1ed4e460fd3ba063b49b55
Nitish Rajopadhyaya <nitishrocker220@gmailcom>
2021-07-27 17:18:54 +0545

    operator overloading with pre - increment

---
+++ lab/lab9_polymorphism/fibonacci_series.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+using namespace std;
+
+class Series
+{
+    private:
+      int num1 , num2 , num3;
+
+    public:
+       Series()
+       {
+           num1=0;
+           num2=1;
+       }   
+
+       void operator ++()
+       {
+           cout<<num1 <<" \t";
+           num3=num1+num2;
+           num1=num2;
+           num2=num3;
+       }
+
+};
+
+int main()
+{
+      Series S1;
+
+       int limit;
+       cout<<"Enter  limit of serires " <<endl;
+       cin>>limit;
+
+      for(int i=0 ;i<limit;i++)
+       {
+           ++S1;
+       }
+}
