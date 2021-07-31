4bf06bb173592c5c9f6c8805addf154ebe8212b6
Nitish Rajopadhyaya <nitishrocker220@gmailcom>
2021-07-31 14:12:54 +0545

    Exception handeling first program

---
+++ class_content/chap_10_exceptionhandeling/exception_handeling1.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int num1, num2;
+    int opertion;
+     cout<<"enter two integers "<<endl;
+    cin>>num1>>num2;
+
+   opertion = (num1/(num1-num2));
+
+   cout<<"value is"<<opertion;
+
+}
